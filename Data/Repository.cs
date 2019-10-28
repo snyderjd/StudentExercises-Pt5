3ff001bd741d7523407527a64956458428e3d406
Joseph Snyder <snyderjosephd@yahoo.com>
2019-10-28 09:35:24 -0500

    Created Repository class and Exercise class in Data and Models directories, respectively

---
+++ Data/Repository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace StudentExercises5.Data
+{
+    class Repository
+    {
+        ///<summary>
+        /// Creates a tunnel to connect the application to the database. All communication between the application and the database
+        /// passes through this connection.
+        ///</summary>
+        public SqlConnection Connection
+        {
+            get
+            {
+                // This is the "address" of the database
+                string _connectionString = "Data Source=DESKTOP-TFEHV46\SQLEXPRESS;Initial Catalog=StudentExercises;Integrated Security=True";
+                return new SqlConnection(_connectionString);                
+            }
+        }
+
+        ///<summary>
+        ///Returns a list of all the exercises in the database
+        ///</summary>
+    }
+}
---
+++ Models/Exercise.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentExercises5.Models
+{
+    // ## Exercise
+
+    // You must define a type for representing an exercise in code.An exercise can be assigned to many students.
+
+    // 1. Name of exercise
+    // 1. Language of exercise (JavaScript, Python, CSharp, etc.)
+    class Exercise
+    {
+        public string Name { get; set; }
+        public string Language { get; set; }
+        public Exercise(string name, string language)
+        {
+            Name = name;
+            Language = language;
+        }
+    }
+}
+
+
